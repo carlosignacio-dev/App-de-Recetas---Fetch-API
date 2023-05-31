081c8794c717a84c82b33e266c1ded8e23745a67
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-31 09:28:49 -0400

    added options category

--- js/app.js
+++ js/app.js
@@ -2,13 +2,26 @@
 
 function iniciarApp() {
 
+    const selectCategorias = document.querySelector("#categorias");
+
     obtenerCategorias();
     
     function obtenerCategorias() {
         const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
         fetch(url)
             .then(response => response.json())
-            .then(resultado => console.log(resultado))
+            .then(resultado => mostrarCategorias(resultado.categories))
+    }
+
+    function mostrarCategorias(categorias = []) {
+        categorias.forEach(categoria => {
+
+            const { strCategory } = categoria;
+            const option = document.createElement('option');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategorias.appendChild(option);            
+        })
     }
 }
 
